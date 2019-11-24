0d04a5277dd34245a20e4ac10fbaf48fc20616f4
Amar Jasarbasic <amarjasarbasic@gmail.com>
2019-11-24 15:05:58 -0500

    added service folder

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
+import { Student } from '../model/student';
+
+@Injectable()
+export class StudentService {
+
+    constructor() {
+
+    }
+
+    getStudentId() {
+
+    }
+}
