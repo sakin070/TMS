281be7a83d8d939d5a8d548e77e677f4bcc1379e
Saheed Akinbile <sakin070@uottawa.ca>
2019-11-29 19:25:47 -0500

    added professor component and appropriate routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import {RegistrationFormComponent} from './registration-form/registration-form.c
 import {LoginComponent} from './login/login.component';
 import { StudentComponent } from './student/student.component';
 import { TeamsListComponent } from './teams-list/teams-list.component';
+import {ProfessorComponent} from './professor/professor.component';
+import {CourseListComponent} from './course-list/course-list.component';
 
 const routes: Routes = [
   {path: 'register', component: RegistrationFormComponent},
   {path: 'login', component: LoginComponent},
   {path: 'student', component: StudentComponent},
   {path: 'teams', component: TeamsListComponent},
+  {path: 'professor', component: ProfessorComponent},
+  {path: 'courses', component: CourseListComponent},
   ];
 
 @NgModule({
